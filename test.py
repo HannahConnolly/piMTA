61222052e23e0ccebbeac5d5deb46dcb558d9147
HannahConnolly <hannahconnolly@Hannahs-MBP.lan>
2025-03-11 03:13:25 -0400

    removed test file

--- test.py
+++
@@ -1,2 +0,0 @@
-{'M': 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm', 'J': 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-jz'}
-{'M': None, 'J': None}
